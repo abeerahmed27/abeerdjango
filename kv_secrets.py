e3530f9453900859ab695684caf14b3e06f050d9
abeerahmed27 <74042554+abeerahmed27@users.noreply.github.com>
2023-03-01 14:02:22 +0000

    Add files via upload

---
+++ kv_secrets.py
@@ -0,0 +1,27 @@
+import os
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+
+keyVaultName = "abeerkeyvault"
+KVUri = f"https://abeerkeyvault.vault.azure.net/"
+
+credential = DefaultAzureCredential()
+client = SecretClient(vault_url=KVUri, credential=credential)
+
+secretName = input("Input a name for your secret > ")
+secretValue = input("Input a value for your secret > ")
+
+print(f"Creating a secret in {keyVaultName} called '{secretName}' with the value '{secretValue}' ...")
+
+client.set_secret(secretName, secretValue)
+
+print(" done.")
+
+print(f"Retrieving your secret from {keyVaultName}.")
+
+retrieved_secret = client.get_secret(secretName)
+
+print(f"Your secret is '{retrieved_secret.value}'.")
+
+
+print(" done.")
